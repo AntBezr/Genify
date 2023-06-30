80adf21de258df1460d44f1c9f0d9c8a82a25f11
AntBezr <sosleepyanton@gmail.com>
2023-06-30 14:10:02 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import headerCss from './css/Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={headerCss.header}>
+      Header
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/SongPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SongPage = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default SongPage;
